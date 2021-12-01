4bdf53cb144a0df4eda5bd290220bac816c56dfa
Giovanni Gaffe <gaffejonathan@ymail.com>
2021-12-01 11:14:36 +0800

    set accessibility in different file

--- AccessibilityProject/ContentView.swift
+++ AccessibilityProject/ContentView.swift
@@ -8,37 +8,13 @@
 import SwiftUI
 
 struct ContentView: View {
-    let pictures = [
-        "ales-krivec-15949",
-        "galina-n-189483",
-        "kevin-horstmann-141705",
-        "nicolas-tissot-335096"
-    ]
-    
-    
-    let labels = [
-        "Tulips",
-        "Frozen tree buds",
-        "Sunflowers",
-        "Fireworks"
-    ]
-    @State private var selectedPicture = Int.random(in: 0...3)
-    
     var body: some View {
-        Image(pictures[selectedPicture])
-            .resizable()
-            .scaledToFit()
-            .accessibility(label: Text(labels[selectedPicture]))
-            .accessibility(addTraits: .isButton)
-            .accessibility(removeTraits: .isImage)
-            .onTapGesture {
-                self.selectedPicture = Int.random(in: 0...3)
-            }
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        AccessibilityModifier()
     }
 }
