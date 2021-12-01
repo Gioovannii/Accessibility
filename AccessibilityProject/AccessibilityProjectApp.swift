e8087c323328fa11929ec9693b8b16c2c6b2439d
Giovanni Gaffe <gaffejonathan@ymail.com>
2021-12-01 10:56:06 +0800

    Initial Commit

---
+++ AccessibilityProject/AccessibilityProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  AccessibilityProjectApp.swift
+//  AccessibilityProject
+//
+//  Created by Giovanni Gaffé on 2021/12/1.
+//
+
+import SwiftUI
+
+@main
+struct AccessibilityProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
